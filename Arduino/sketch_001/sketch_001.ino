1187d8d682c12d463eb5cb2927c0d5cb8edfd1c8
oniontastegarlic <oniontastegarlic@gmail.com>
2015-03-05 15:33:25 +0900

    led blink

--- Arduino/sketch_001/sketch_001.ino
+++ Arduino/sketch_001/sketch_001.ino
@@ -1,9 +1,13 @@
+int led = A1;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(led,HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
 }
